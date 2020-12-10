d28750c4e042c960dee36acbba4762ebba8bf870
Ashley Horton <ashley@ashleyhorton.net>
2020-12-10 09:07:41 -0700

    add offset polygon

---
+++ Nez.Portable/Utils/OffsetPolygon.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Nez.PhysicsShapes;
+
+namespace Nez
+{
+    public class OffsetPolygon : Polygon
+    {
+        public Vector2 Offset { get; set; }
+
+        public OffsetPolygon(Vector2[] points, Vector2 offset) : base(points)
+        {
+            Offset = offset;
+        }
+        
+        public OffsetPolygon(Vector2[] points) : base(points)
+        {
+        }
+
+        public OffsetPolygon(int vertCount, float radius) : base(vertCount, radius)
+        {
+        }
+    }
+}
